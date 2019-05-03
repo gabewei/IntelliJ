a50fdf3872c6d264c5643b7b1aa7e7998b86ab50
gabewei <ggdoubleu@gmail.com>
2019-05-03 13:14:06 -0600

    Friday

--- MultiUserChat/ChatServer/src/com/muc/ServerMain.java
+++ MultiUserChat/ChatServer/src/com/muc/ServerMain.java
@@ -1,24 +1,9 @@
 package com.muc;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.io.IOException;
-
 public class ServerMain {
     public static void main(String[] args) {
         int port = 8818;
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
-            while(true) {
-                System.out.println("About to accept client connection...");
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Accepted connection from " + clientSocket);
-                ServerWorker worker = new ServerWorker(clientSocket);
-                worker.start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Server server = new Server(port);
+        server.start();
     }
-
 }
